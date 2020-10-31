61fdca54f32c4f16c94c3e259bb3d2f803ec1428
xdayi <xdayi@linewell.com>
2020-10-31 15:49:27 +0800

    初步搭建

---
+++ qrdefray/src/main/java/com/sewevi/qrdefray/QRDefrayApplication.java
@@ -0,0 +1,12 @@
+package com.sewevi.qrdefray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QRDefrayApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(QRDefrayApplication.class, args);
+    }
+}
